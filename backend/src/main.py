ca0462b3a6eb660672d907b75d16994aa62b36a6
tyshbyr <78562711+tyshbyr@users.noreply.github.com>
2023-07-28 17:08:07 +0500

    Fixed skipping failed tests in Actions (#19)

--- backend/src/main.py
+++ backend/src/main.py
@@ -10,17 +10,18 @@ from backend.src.api.v1 import api_likes, api_reviews
 from backend.src.api.v1.api_bookmarks import router as bookmarks_router
 from backend.src.auth import rsa_key
 from backend.src.auth.abc_key import RsaKey
-from backend.src.core.config import DSN, PUBLIC_KEY, AppSettings
+from backend.src.core.config import DSN, PUBLIC_KEY, config
 from backend.src.core.logger import LOGGING
 from backend.src.db import mongo_db
 from backend.src.db.storage import MongoStorage
 
-sentry_sdk.init(dsn=DSN, integrations=[FastApiIntegration()])
 
-logging.basicConfig(**LOGGING)
-log = logging.getLogger(__name__)
+if config.logging_on:
+    sentry_sdk.init(dsn=DSN, integrations=[FastApiIntegration()])
 
-config = AppSettings()
+    logging.basicConfig(**LOGGING)
+    log = logging.getLogger(__name__)
+
 app = FastAPI(
     title=config.project_name,
     docs_url="/api/openapi",
@@ -31,24 +32,21 @@ app = FastAPI(
 
 @app.on_event("startup")
 async def startup():
-    mongo_db.mongo = MongoStorage(
-        host=config.mongo_host, port=config.mongo_port)
+    mongo_db.mongo = MongoStorage(host=config.mongo_host, port=config.mongo_port)
     rsa_key.pk = RsaKey(path=PUBLIC_KEY, algorithms=["RS256"])
 
 
 @app.on_event("shutdown")
 async def shutdown():
     if mongo_db.mongo:
-        await mongo_db.mongo.close()  # type: ignore
+        await mongo_db.mongo.close()
 
-app.include_router(
-    api_reviews.router, prefix="/api/v1/reviews", tags=["reviews"])
+
+app.include_router(api_reviews.router, prefix="/api/v1/reviews", tags=["reviews"])
 
 app.include_router(api_likes.router, prefix="/api/v1/likes", tags=["likes"])
 
-app.include_router(
-    bookmarks_router, prefix="/api/v1/bookmarks", tags=["bookmarks"])
+app.include_router(bookmarks_router, prefix="/api/v1/bookmarks", tags=["bookmarks"])
 
 if __name__ == "__main__":
-
     uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
